95b27860188d3e68018e6eb18b8e0d3b915371b4
JoJoWitness <Jordano470@gmail.com>
2024-06-06 22:35:22 -0400

    Added Ariani's advances

---
+++ Parcial III/Tema I/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+struct Node{
+    string inf;
+    Node *hi,*hd;
+
+    // constructor 
+    Node(string val): inf(val), hi(NULL), hd(NULL){}
+};
+
+// funcion para insertar nodos al ABB
+void insertNode(Node *&tree, string inf){
+    if(tree == NULL){
+        Node *new_node = new Node(inf); 
+        tree = new_node;
+    }else{
+        string root = tree -> inf;
+        if(inf<root){
+            insertNode(tree->hi,inf);
+        }
+        else{
+            insertNode(tree->hd,inf);
+        }
+    }
+}
+
+// Funcion recorrido postorden
+void postOrder(Node * tree){
+    if(tree==NULL){
+        return;
+    }else{
+        postOrder(tree->hi);
+        postOrder(tree->hd);
+        cout<<tree->inf<<" - ";
+    }
+}
+
+// Calcular altura
+int treeHeight(Node * tree){
+    if(tree==NULL){
+        return 1;
+    }else{
+        int left_h = treeHeight(tree->hi);
+        int right_h = treeHeight(tree->hd);
+        return (left_h>right_h ? left_h:right_h)+1;
+    }
+}
+
+// Cantidad de hojas 
+
+// Verificar arbol equilibrado
+
+// Iniciar ABB
+Node *tree = NULL;
+
+int main(){
+    string word;
+    string sentence;
+    ifstream treetxt;
+    treetxt.open("arbol.txt");
+
+    if (treetxt.fail()) {
+        cerr << "Error al leer el archivo." << endl;
+        return 1;
+    }
+
+    while(!treetxt.eof()){
+        //treetxt.getline(sentence,LENGHT,'\n');
+        getline(treetxt, sentence);
+        istringstream iss(sentence);
+        
+        while(iss >> word){
+            insertNode(tree,word);
+        }
+
+    }
+
+    treetxt.close();
+    return 0
